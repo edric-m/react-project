14b228bd5c8a73701ce06f42faabbfeb2109b507
edric <edjomendoza@gmail.com>
2019-12-03 14:46:27 +1100

    promising results

--- src/components/audioIn.js
+++ src/components/audioIn.js
@@ -22,21 +22,22 @@ class AudioIn extends React.Component {
     addHarmonics (x, freq) {
         let N = x.length;
         let total = 0;
-        let max = 0;
+        //let max = 0;
         for (let k = 1; (k * freq) < N; k*=2) {
             let re = 0;
             let im = 0;
             for (let n = 0; n < N; n++) {
                 const angle = (Math.PI * 2 * freq * k * n) / N;
                 re += x[n] * Math.cos(angle);
-                //im += x[n] * Math.sin(angle);
+                im -= x[n] * Math.sin(angle);
             }
-            re = re/N;
+            //re = re/N;
             //im = im/N;
-            total += re - im;
-            if (re > max) {
-                max = re;
-            }
+            let amp = (Math.sqrt(Math.pow(re,2) + Math.pow(im, 2)) * 2) / N;
+            total += amp;
+            //if (re > max) {
+            //    max = re;
+            //}
         }
         return total;
     }
@@ -101,16 +102,21 @@ class AudioIn extends React.Component {
     async logStream() {
         let superBuffer = new Blob(recordedChunks);
         let audioData = [];
+        let decodedAudio = [];
         let freq = [];
 
         console.log(recordedChunks);
 
         audioData = await new Response(superBuffer).arrayBuffer();
-        audioData = new Int8Array(audioData);
+        //audioData = new Uint8Array(audioData);
 
-        //console.log(audioData);
+        let ctx = new (window.AudioContext || window.webkitAudioContext)();
+        decodedAudio = await ctx.decodeAudioData(audioData);
 
-        freq = this.dft(audioData);
+        decodedAudio = decodedAudio.getChannelData(0);
+        console.log(decodedAudio);
+
+        freq = this.dft(decodedAudio);
 
         console.log(freq);
     }
@@ -137,7 +143,7 @@ class AudioIn extends React.Component {
             setTimeout(() => {
                 recorder.stop();
                 console.log("recording stop");
-            }, 5000);
+            }, 8000);
 
         });
 
